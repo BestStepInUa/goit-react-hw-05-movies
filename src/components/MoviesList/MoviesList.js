3ef5cfab0a62b264598e64eed34f69cb42460e38
BestStepInUa <beststep.in.ua@gmail.com>
2024-01-20 10:55:29 +0200

    fix v1

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,12 +1,15 @@
-const { Link } = require('react-router-dom');
+import { Link, useLocation } from 'react-router-dom';
 
 const MoviesList = ({ movies }) => {
+  const location = useLocation();
   return (
     <ul>
       {movies.map(({ id, title }) => {
         return (
           <li key={id}>
-            <Link to={`/movies/${id}`}>{title}</Link>
+            <Link to={`/movies/${id}`} state={{ from: location }}>
+              {title}
+            </Link>
           </li>
         );
       })}
